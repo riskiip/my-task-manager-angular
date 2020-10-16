469407af460b2a58c0c3b51ce8d220112bb28829
riskiip <muhamadrizki19@student.undip.ac.id>
2020-10-16 17:38:28 +0700

    Service Get and Post done

--- api/app.js
+++ api/app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 const { Lists, Tasks } = require('./db/models');
+const { mongoose } = require('./db/mongoose');
+
+// Declare the Middleware
+app.use(bodyParser.json());
 
 // Routing the API
 app.get('/lists', (req, res) => {
-    res.send("Hello world");
+    Lists.find().then((lists) => {
+        res.send(lists);
+    }).catch((e) => {
+        res.send("Error: ", e)
+    })
 })
 
 app.post('/lists', (req, res) => {
-
+    let title = req.body.title;
+    let newList = new Lists({
+        title
+    });
+    newList.save().then((listDoc) => {
+        res.send(listDoc);
+    })
 })
 
 app.patch("/lists/:id", (req, res) => {
